db8878e72741a47cc2ca53c0f77e4f2f5b5f5099
D <gdongcode@gmail.com>
2022-05-13 01:39:22 +0800

    feat: 支持打包后analyze

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,33 @@
 const lessPlugin = require('craco-less');
-module.exports = {
-  plugins: [
-    {
-      // 支持less
-      plugin: lessPlugin,
+const AnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const createWebpackPlugins = () => {
+  const isAnalyze = process.env.ANALYZE;
+  const defaultPlugins = [];
+
+  if (isAnalyze) {
+    defaultPlugins.push(
+      new AnalyzerPlugin({
+        analyzerMode: 'server',
+      }),
+    );
+  }
+  return defaultPlugins;
+};
+const createPlugins = () => {
+  const defaultPlugins = [lessPlugin];
+
+  return defaultPlugins.map(plugin => {
+    return {
+      plugin,
+    };
+  });
+};
+module.exports = () => {
+  return {
+    webpack: {
+      plugins: createWebpackPlugins(),
     },
-  ],
+    plugins: createPlugins(),
+  };
 };
