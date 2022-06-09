5cda962fc0624c8dd5eb0035c02951b5edf2b3cf
new-bee <gdongcode@gmail.com>
2022-06-09 15:42:09 +0800

    fix: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: ['react-app', 'react-app/jest', 'prettier'],
   rules: {
-    'no-unused-vars': 'off',
     'max-len': [1, 200],
     'prefer-const': 'error',
     eqeqeq: 'error',
