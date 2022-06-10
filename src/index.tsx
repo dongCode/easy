5c8642ff93d02517f0f93cff54412e0f5e79f602
new-bee <gdongcode@gmail.com>
2022-06-10 18:13:05 +0800

    fix: react-18

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
-import './index.css';
+import '@/styles/index.less';
 import 'dayjs/locale/zh-cn';
 
 import * as dayjs from 'dayjs';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import { App } from '@/pages/app';
+
 dayjs.locale('zh-cn');
+const container = document.getElementById('root');
+if (container === null) throw new Error('Root container missing in index.html');
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(container);
+
+// Initial render
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
